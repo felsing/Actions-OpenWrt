852bc499890981578f7651874a598c8cd177c52c
felsing <59762607+felsing@users.noreply.github.com>
2022-12-19 20:15:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,3 @@ sed -i 's/192.168.1.1/192.168.3.10/g' package/base-files/files/bin/config_genera
 
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
-
-#Fix aarch64 python3(temporary)
-#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=4.8/g' tools/sed/Makefile
-#sed -i 's/PKG_HASH:=.*/PKG_HASH:=f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633/g' tools/sed/Makefile
